b2b1d355dbcdc104d912841c7fb63fbb2841e428
Tennyson Holloway <tennyson.holloway@gmail.com>
2016-11-03 18:49:53 -0700

    office hours

--- page-contact.php
+++ page-contact.php
@@ -28,42 +28,66 @@ get_header(); ?>
                             	                <span>General Manager</span>
                                                 <span>gm@ksdt.org</span>
                                             </td>
-                            	            <td>Susan Bystrom, Tennyson Holloway</td>
+                            	            <td>
+												<span>Susan Bystrom, Tennyson Holloway</span>
+												<span>Friday 9am-10am, 11am-12pm | Thursday 1pm-3pm</span>
+											</td>
                             	        </tr>
                             	        <tr>
                             	            <td><span>Music Director</span><span>music@ksdt.org</span></td>
-                            	            <td>Patty Cuen, Emmet Webster</td>
+                            	            <td>
+												<span>Patty Cuen, Emmet Webster</span>
+												<span>Wednesday 3pm-4pm, Friday 3pm-4pm | Friday 4pm-6pm</span>
+											</td>
                             	        </tr>
                             	        <tr>
                             	            <td><span>Promotions</span><span>promotions@ksdt.org</span></td>
-                            	            <td>Karen Liou</td>
+                            	            <td>
+												<span>Karen Liou</span>
+												<span>Tuesday 11am-1pm</span>
+											</td>
                             	        </tr>
                             	        <tr>
                             	            <td><span>Programming</span><span>programming@ksdt.org</span></td>
-                            	            <td>Anser Abbas, Brandon Slater</td>
+                            	            <td>
+												<span>Anser Abbas, Brandon Slater</span>
+												<span>Tuesday 2pm-3pm, Thursday 2pm-3pm</span>
+											</td>
                             	        </tr>
                             	        <tr>
                             	            <td><span>Graphics</span><span>graphics@ksdt.org</span></td>
-                            	            <td>Nariman Piri</td>
+                            	            <td>
+												<span>Nariman Piri</span>
+												<span>Monday 4pm-5pm, Wednesday 4pm-5pm, Friday 4pm-5pm</span>
+											</td>
                             	        </tr>
                             	        <tr>
                             	            <td><span>Engineer</span><span>engineer@ksdt.org</span></td>
-                            	            <td>Caleb Hess, Sanjay John</td>
+                            	            <td>
+												<span>Caleb Hess, Sanjay John</span>
+												<span>Friday 1pm-3pm | ?</span>
+											</td>
                             	        </tr>
                             	        <tr>
                             	            <td><span>Technology</span><span>webmaster@ksdt.org</span></td>
-                            	            <td>Raymond Arevalo</td>
+                            	            <td>
+												<span>Raymond Arevalo</span>
+												<span>?</span>
+											</td>
                             	        </tr>
                             	        <tr>
                             	            <td><span>Secretary</span><span>secretary@ksdt.org</span></td>
-                            	            <td>Zachary Neumann</td>
+                            	            <td>
+												<span>Zachary Neumann</span>
+												<span>Monday 2pm-4pm</span>
+											</td>
                             	        </tr>
                             	    </tbody>
                         	    </table>
 
                     	    </div>
                     	    <div class="col-lg-6 col-md-12">
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d418.8490460883834!2d-117.24053723174286!3d32.877212581100885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06c5852f9e21%3A0x2154b8e9cbc32568!2sKSDT+Radio!5e0!3m2!1sen!2sus!4v1478220664200" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>  
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d418.8490460883834!2d-117.24053723174286!3d32.877212581100885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06c5852f9e21%3A0x2154b8e9cbc32568!2sKSDT+Radio!5e0!3m2!1sen!2sus!4v1478220664200" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
                   	        <pre>Attn: KSDT RADIO
 University of California, San Diego
 9500 Gilman Dr. #0077
