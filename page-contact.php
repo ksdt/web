ec0ec3d819f3aa1b4887dac58950ef96e59444c9
Tennyson Holloway <tennyson.holloway@gmail.com>
2016-11-03 17:54:13 -0700

    added official ksdt goog bus map

--- page-contact.php
+++ page-contact.php
@@ -60,11 +60,11 @@ get_header(); ?>
                             	        </tr>
                             	    </tbody>
                         	    </table>
-                             
+
                     	    </div>
                     	    <div class="col-lg-6 col-md-12">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d837.6975779161281!2d-117.24075017079525!3d32.877266998808864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzLCsDUyJzM4LjIiTiAxMTfCsDE0JzI0LjciVw!5e0!3m2!1sen!2sus!4v1473031935948" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>                    	        <h4>Mail Us</h4>
-                    	        <pre>Attn: KSDT RADIO
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d418.8490460883834!2d-117.24053723174286!3d32.877212581100885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc06c5852f9e21%3A0x2154b8e9cbc32568!2sKSDT+Radio!5e0!3m2!1sen!2sus!4v1478220664200" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>  
+                  	        <pre>Attn: KSDT RADIO
 University of California, San Diego
 9500 Gilman Dr. #0077
 La Jolla, CA. 92093-0315</pre>
