6a0a729ef31a7269a81a87ab608d27f693489c9c
Bijan Farahani <bijancfarahani@gmail.com>
2017-10-11 13:57:35 -0700

    changed my office hours

--- page-contact.php
+++ page-contact.php
@@ -72,7 +72,7 @@ get_header(); ?>
                             	            <td><span>Technology</span><span>webmaster@ksdt.org</span></td>
                             	            <td>
 												<span>Bijan Farahani</span>
-												<span>Thursday 1pm-3pm</span>
+												<span>Tuesday 2pm-4pm</span>
 											</td>
                             	        </tr>
                             	        <tr>
